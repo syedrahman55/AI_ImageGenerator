8060e55a17f0dbcabfcde787faee1bb0b54cd643
Syed Rahman <syedrahman498@gmail.com>
2023-01-08 00:22:33 +0530

    first commit

--- controllers/OpenaiController.js
+++ controllers/OpenaiController.js
@@ -19,6 +19,7 @@ const generateImage = async (req, res) => {
         });
 
         const imageUrl = response.data.data[0].url;
+        
         res.status(200).json({
             success:true,
             data:imageUrl,
@@ -34,7 +35,7 @@ const generateImage = async (req, res) => {
 
         res.status(400).json({
             success: false,
-            error:'The image could not be generated'
+            error:'The image could not be generated',
         }); 
     }
 };
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -9,6 +10,12 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+
+//Set static folder 
+app.use(express.static(path.join(__dirname,'public')));
+
+
 app.use('/openai',require('./routes/openaiRoutes'));
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
